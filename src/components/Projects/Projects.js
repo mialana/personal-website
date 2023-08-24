5f579b3400442566115d726d4b07f512fb73e864
mialana <96770662+mialana@users.noreply.github.com>
2023-08-24 15:39:48 -0700

    lint 2

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,4 +1,4 @@
-import { makeStyles } from '@material-ui/core/styles';
+// import { makeStyles } from '@material-ui/core/styles';
 import React, { useContext } from 'react';
 
 import { ThemeContext } from '../../contexts/ThemeContext';
@@ -12,32 +12,32 @@ function Projects() {
     const { theme } = useContext(ThemeContext);
 
 
-    const useStyles = makeStyles(() => ({
-        viewAllBtn: {
-            color: theme.secondary,
-            backgroundColor: theme.buttonColor,
-            transition: 'color 0.5s',
-            "&:hover": {
-                color: theme.secondary,
-                backgroundColor: theme.primary,
-            }
-        },
-        viewArr: {
-            color: theme.buttonColor,
-            backgroundColor: theme.secondary,
-            width: '40px',
-            height: '40px',
-            padding: '0.5rem',
-            fontSize: '1.05rem',
-            borderRadius: '50%',
-            cursor: 'pointer',
-            transition: 'background-color 0.5s',
-            "&:hover": {
-                color: theme.buttonColor,
-                backgroundColor: theme.secondary,
-            }
-        },
-    }));
+    // const useStyles = makeStyles(() => ({
+    //     viewAllBtn: {
+    //         color: theme.secondary,
+    //         backgroundColor: theme.buttonColor,
+    //         transition: 'color 0.5s',
+    //         "&:hover": {
+    //             color: theme.secondary,
+    //             backgroundColor: theme.primary,
+    //         }
+    //     },
+    //     viewArr: {
+    //         color: theme.buttonColor,
+    //         backgroundColor: theme.secondary,
+    //         width: '40px',
+    //         height: '40px',
+    //         padding: '0.5rem',
+    //         fontSize: '1.05rem',
+    //         borderRadius: '50%',
+    //         cursor: 'pointer',
+    //         transition: 'background-color 0.5s',
+    //         "&:hover": {
+    //             color: theme.buttonColor,
+    //             backgroundColor: theme.secondary,
+    //         }
+    //     },
+    // }));
 
     // const classes = useStyles();
 
