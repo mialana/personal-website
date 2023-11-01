7634fda065f5b8ef63719ffa7676b35d8bc40b36
mialana <96770662+mialana@users.noreply.github.com>
2023-11-01 12:21:58 -0400

    updated resume

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -3,5 +3,5 @@ export const headerData = {
     name: 'Amy Liu',
     image: 'https://i.imgur.com/jKnoFd7.jpg',
     description: "Hi! My name is Amy, and I'm currently pursuing an Ivy League undergraduate degree in Computer Science and Digital Media Design (Computer Graphics) at the University of Pennsylvania.",
-    resumePdf: 'https://drive.google.com/file/d/1KSqDsjP627K9nMKhTFlRr8IcWDC6bgBi/view?usp=sharing'
+    resumePdf: 'https://drive.google.com/file/d/1k6zOU8PO3L6vZv18_afFJ75iuTCy0plI/view?usp=sharing'
 }
